b89e12ccb392ab4e8b24ecfbcbc8343a0d0727fe
DoubleDebug <62041862+DoubleDebug@users.noreply.github.com>
2021-02-21 02:50:31 +0100

    Create Node.cs

---
+++ Binary Tree Inversion/Node.cs
@@ -0,0 +1,16 @@
+namespace BinaryTree
+{
+    class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int val)
+        {
+            value = val;
+            left = null;
+            right = null;
+        }
+    }
+}
